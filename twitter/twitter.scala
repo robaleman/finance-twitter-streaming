3a8fe1696890a42d277ea3ce488a843a43deb3b9
Robert Aleman <robert.aleman@pomona.edu>
2017-12-05 02:19:59 -0500

    Update twitter.scala

--- twitter/twitter.scala
+++ twitter/twitter.scala
@@ -1,5 +1,15 @@
-object HelloWorld {
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+
+object Twitter {
   def main(args: Array[String]) {
-    println("Hello, world!")
+
+    val consumerKey = "hidden"
+    val consumerSecret = "hidden"
+    val accessToken = "hidden"
+    val accessTokenSecret = "hidden"
   }
 }
