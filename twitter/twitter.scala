171e3c8295941f3fa4fdb3e971f0bdd0aa74b66b
rob <robert.aleman@pomona.edu>
2017-12-25 23:26:46 -0500

    Update twitter.scala

--- twitter/twitter.scala
+++ twitter/twitter.scala
@@ -1,34 +1,54 @@
-// INCOMPLETE
+import org.apache.spark._
+import org.apache.spark.streaming._
 import org.apache.spark.streaming.twitter._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.slf4j.LoggerFactory
-import scopt.OptionParser
+import twitter4j.auth.OAuthAuthorization
+import twitter4j.conf.ConfigurationBuilder
+
+// add this if you want to disable excess logs in console
+import org.apache.log4j.{Level, Logger}
 
 
 object Twitter {
-  
+
   def main(args: Array[String]) {
 
-    // authenticate using personal Twitter API tokens 
+    // authenticate using personal Twitter API tokens
     val consumerKey = "hidden"
     val consumerSecret = "hidden"
     val accessToken = "hidden"
     val accessTokenSecret = "hidden"
 
-    System.setProperty("twitter4j.oauth.consumerKey", "CONSUMER_API_KEY")
-    System.setProperty("twitter4j.oauth.consumerSecret", "CONSUMER_API_SECRET")
-    System.setProperty("twitter4j.oauth.accessToken", "ACCESS_TOKEN")
-    System.setProperty("twitter4j.oauth.accessTokenSecret", "ACCESS_TOKEN_SECRET")
+    // sets up authentication
+    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+    val auth = Some(new OAuthAuthorization(new ConfigurationBuilder().build()))
 
-    // create stream
-    val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(1))
-    val tweets = TwitterUtils.createStream(ssc, None)
 
+    // creates new Spark streaming context
+    val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(30))
+
+    // creates a Twitter stream to use as input source
+    val tweets = TwitterUtils.createStream(ssc, auth)
+
+    // extracts text from incoming tweets
     val statuses = tweets.map(status => status.getText())
-    statuses.print()
 
-    // start running stream
+    // filters tweets that contain certain keywords and prints to console
+    val btc_tweets = statuses.filter(status => status contains "bitcoin")
+    btc_tweets.print()
+
+    // OPTIONAL: toggle these to disable excess messages in console
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)
+
     ssc.start()
     ssc.awaitTermination()
   }
+ 
 }
+
+
+
